63de25dc666cc10569660c34540a89a84c2de7c0
zfz <zhoufangzhang@gmail.com>
2016-12-25 03:01:02 +0000

    Change code indent for assignment 3

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -11,85 +11,85 @@ angular.module("NarrowItDownApp", [])
 
 NarrowItDownController.$inject = ["MenuSearchService"];
 function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
+  var ctrl = this;
 
-    ctrl.searchTerm = "";
-    ctrl.found = null;          // We will use 'null' as an indicator of the fact
-                                // that we haven't yet searched for anything.
+  ctrl.searchTerm = "";
+  ctrl.found = null;      // We will use 'null' as an indicator of the fact
+                          // that we haven't yet searched for anything.
 
-    ctrl.getMatchedMenuItems = function() {
-        if (ctrl.searchTerm.trim() === "") {
-            ctrl.found = [];
-        } else {
-            ctrl.found = null;
-            ctrl.searching = true;
+  ctrl.getMatchedMenuItems = function() {
+    if (ctrl.searchTerm.trim() === "") {
+      ctrl.found = [];
+    } else {
+      ctrl.found = null;
+      ctrl.searching = true;
 
-            MenuSearchService.getMatchedMenuItems(ctrl.searchTerm).then(function(found) {
-                ctrl.found = found;
-                ctrl.searching = false;
-            });
-        }
-    };
+      MenuSearchService.getMatchedMenuItems(ctrl.searchTerm).then(function(found) {
+        ctrl.found = found;
+        ctrl.searching = false;
+      });
+    }
+  };
 
-    ctrl.removeMatchedMenuItem = function(index) {
-        this.found.splice(index, 1);
-    };
+  ctrl.removeMatchedMenuItem = function(index) {
+    this.found.splice(index, 1);
+  };
 }
 
 MenuSearchService.$inject = ["$http", "URL"];
 function MenuSearchService($http, URL) {
-    var service = this;
+  var service = this;
 
-    service.getMatchedMenuItems = function(searchTerm) {
-        return $http({
-            method: "GET",
-            url: URL
-        }).then(function(response) {
-            var allItems = response.data.menu_items, matchedItems = [];
+  service.getMatchedMenuItems = function(searchTerm) {
+    return $http({
+      method: "GET",
+      url: URL
+    }).then(function(response) {
+      var allItems = response.data.menu_items, matchedItems = [];
 
-            allItems.forEach(function(item) {
-                if (item.description && (item.description.indexOf(searchTerm) !== -1)) {
-                    matchedItems.push(item);
-                }
-            });
+      allItems.forEach(function(item) {
+        if (item.description && (item.description.indexOf(searchTerm) !== -1)) {
+          matchedItems.push(item);
+        }
+      });
 
-            return matchedItems;
-        }).catch(function(response) {
-            console.error(response);
-        })
-    };
+      return matchedItems;
+    }).catch(function(response) {
+      console.error(response);
+    })
+  };
 
-    return service;
+  return service;
 }
 
 FoundItemsDirective.$inject = [];
 function FoundItemsDirective() {
-    var ddo = {
-        restrict: "E",
-        templateUrl: "foundItems.html",
-        scope: {
-            items: "<",
-            highlight: "<",
-            onRemove: "&"
-        },
-        controller: FoundItemsDirectiveController,
-        controllerAs: "ctrl",
-        bindToController: true
-    };
+  var ddo = {
+    restrict: "E",
+    templateUrl: "foundItems.html",
+    scope: {
+      items: "<",
+      highlight: "<",
+      onRemove: "&"
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: "ctrl",
+    bindToController: true
+  };
 
-    function FoundItemsDirectiveController() {
-        // Nothing here, really... :-)
-    };
+  function FoundItemsDirectiveController() {
+    // Nothing here, really... :-)
+  };
 
-    return ddo;
+  return ddo;
 }
 
 HighlightFilterFactory.$inject = ["$sce"];
 function HighlightFilterFactory($sce) {
-    return function(value, toHighlight) {
-        return $sce.trustAsHtml(
-                value.replace(toHighlight, "<span class=\"bg-info\">" + toHighlight + "</span>"));
-    };
+  return function(value, toHighlight) {
+    return $sce.trustAsHtml(
+        value.replace(toHighlight, "<span class=\"bg-info\">" + toHighlight + "</span>"));
+  };
 }
 
 })();
